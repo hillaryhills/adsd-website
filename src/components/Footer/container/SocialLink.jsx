6edd72cb247465269ea4f987934f0b4cb317a7d3
Hyginus Ugwumba <hyginus.ug@gmail.com>
2020-05-05 17:37:56 +0800

    update footer

--- src/components/Footer/container/SocialLink.jsx
+++ src/components/Footer/container/SocialLink.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { SocialIcon } from './footer.style'
+import { Facebook } from '../../../assets/social/facebook.svg'
+import { Instagram } from '../../../assets/social/instagram.svg'
+import { Twitter } from '../../../assets/social/twitter.svg'
 
-function SocialLink({ link }) {
+const icons = {
+  facebook: Facebook,
+  twitter: Twitter,
+  instagram: Instagram,
+}
+
+export default function SocialLink({ link }) {
   return (
-    <SocialIcon href={link.href} target="_blank">
-      {(<Twitter />)[link.title]}
+    <SocialIcon
+      href={link.href}
+      target="_blank"
+      data-key={`footer__social-icons__${link.title}`}
+    >
+      {React.createElement(icons[link.title], { size: 'sm' })}
     </SocialIcon>
   )
 }
-export default SocialLink
